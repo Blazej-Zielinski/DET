219e2e2a81adab0e89ef12d5f2adc64709cbb7b5
Szymon Sciegienny <szymon.sciegienny@codlake.com>
2024-05-27 14:40:28 +0200

    fixed AADE to be consistent with paper

--- diffEvoLib/diffEvoAlgs/aade.py
+++ diffEvoLib/diffEvoAlgs/aade.py
@@ -1,7 +1,7 @@
 from diffEvoLib.diffEvoAlgs.base import BaseDiffEvoAlg
 from diffEvoLib.diffEvoAlgs.data.alg_data import AADEData
-from diffEvoLib.diffEvoAlgs.methods.methods_default import mutation, binomial_crossing
-from diffEvoLib.diffEvoAlgs.methods.methods_aade import aade_selection, aade_adapat_parameters
+from diffEvoLib.diffEvoAlgs.methods.methods_aade import aade_mutation, aade_crossing, aade_selection, \
+    aade_adapat_parameters
 from diffEvoLib.models.enums.boundary_constrain import fix_boundary_constraints
 
 
@@ -9,29 +9,26 @@ class AADE(BaseDiffEvoAlg):
     def __init__(self, params: AADEData, db_conn=None, db_auto_write=False):
         super().__init__(AADE.__name__, params, db_conn, db_auto_write)
 
-        self.mutation_factor = params.mutation_factor  # F
-        self.mutation_factors = []
-        self.crossover_rate = params.crossover_rate  # Cr
-        self.crossover_rates = []
+        self.mutation_factors = [[params.mutation_factor, False] for _ in range(params.population_size)]
+        self.crossover_rates = [[params.crossover_rate, False] for _ in range(params.population_size)]
 
     def next_epoch(self):
         # New population after mutation
-        v_pop = mutation(self._pop, f=self.mutation_factor)
+        v_pop = aade_mutation(self._pop, mutation_factors=self.mutation_factors)
 
         # Apply boundary constrains on population in place
         fix_boundary_constraints(v_pop, self.boundary_constraints_fun)
 
         # New population after crossing
-        u_pop = binomial_crossing(self._pop, v_pop, cr=self.crossover_rate)
+        u_pop = aade_crossing(self._pop, v_pop, crossover_rates=self.crossover_rates)
 
         # Update values before selection
         u_pop.update_fitness_values(self._function.eval)
 
         # Select new population
-        new_pop = aade_selection(self._pop, u_pop, self.mutation_factors, self.crossover_rates, self.mutation_factor,
-                                 self.crossover_rate)
+        new_pop = aade_selection(self._pop, u_pop, self.mutation_factors, self.crossover_rates)
 
-        self.mutation_factor, self.crossover_rate = aade_adapat_parameters(self.mutation_factors, self.crossover_rates)
+        aade_adapat_parameters(self._pop, self.mutation_factors, self.crossover_rates)
         # Override data
         self._pop = new_pop
 
--- diffEvoLib/diffEvoAlgs/methods/methods_aade.py
+++ diffEvoLib/diffEvoAlgs/methods/methods_aade.py
@@ -1,14 +1,59 @@
 import numpy as np
-from math import floor
 import random
 import copy
 
+from diffEvoLib.diffEvoAlgs.methods.methods_default import mutation_ind, binomial_crossing_ind
 from diffEvoLib.models.population import Population
 from diffEvoLib.models.enums.optimization import OptimizationType
 
 
-def aade_selection(origin_population: Population, modified_population: Population, mutation_factors: list[float],
-                   crossover_rates: list[float], f: float, cr: float) -> Population:
+def aade_mutation(population: Population, mutation_factors: list[list[float, bool]]) -> Population:
+    new_members = []
+    members = population.members
+
+    for i in range(population.size):
+        idxs = np.random.choice(population.size, 3, replace=False)
+        selected_members = members[idxs]
+        new_member = mutation_ind(selected_members[0], selected_members[1], selected_members[2], mutation_factors[i][0])
+        new_members.append(new_member)
+
+    new_population = Population(
+        interval=population.interval,
+        arg_num=population.arg_num,
+        size=population.size,
+        optimization=population.optimization
+    )
+    new_population.members = np.array(new_members)
+    return new_population
+
+
+def aade_crossing(origin_population: Population, mutated_population: Population,
+                  crossover_rates: list[list[float, bool]]) -> Population | None:
+    if origin_population.size != mutated_population.size:
+        print("Binomial_crossing: populations have different sizes")
+        return None
+
+    new_members = []
+
+    for i in range(origin_population.size):
+        new_member = binomial_crossing_ind(origin_population.members[i],
+                                           mutated_population.members[i],
+                                           crossover_rates[i][0])
+        new_members.append(new_member)
+
+    new_population = Population(
+        interval=origin_population.interval,
+        arg_num=origin_population.arg_num,
+        size=origin_population.size,
+        optimization=origin_population.optimization
+    )
+    new_population.members = np.array(new_members)
+    return new_population
+
+
+def aade_selection(origin_population: Population, modified_population: Population,
+                   mutation_factors: list[list[float, bool]],
+                   crossover_rates: list[list[float, bool]]) -> Population | None:
     if origin_population.size != modified_population.size:
         print("Selection: populations have different sizes")
         return None
@@ -23,25 +68,23 @@ def aade_selection(origin_population: Population, modified_population: Populatio
         if optimization == OptimizationType.MINIMIZATION:
             if origin_population.members[i] <= modified_population.members[i]:
                 new_members.append(copy.deepcopy(origin_population.members[i]))
-                mutation_factors.append(
-                    f - (modified_population.members[i].fitness_value - origin_population.members[i].fitness_value) /
-                    modified_population.members[i].fitness_value * random.random())
-
-                crossover_rates.append(
-                    cr - (modified_population.members[i].fitness_value - origin_population.members[i].fitness_value) /
-                    modified_population.members[i].fitness_value * random.random())
+                mutation_factors[i] = [mutation_factors[i][0] - (
+                        modified_population.members[i].fitness_value - origin_population.members[i].fitness_value) /
+                                       modified_population.members[i].fitness_value * random.random(), True]
+                crossover_rates[i] = [crossover_rates[i][0] - (
+                        modified_population.members[i].fitness_value - origin_population.members[i].fitness_value) /
+                                      modified_population.members[i].fitness_value * random.random(), True]
             else:
                 new_members.append(copy.deepcopy(modified_population.members[i]))
         elif optimization == OptimizationType.MAXIMIZATION:
             if origin_population.members[i] >= modified_population.members[i]:
                 new_members.append(copy.deepcopy(origin_population.members[i]))
-                mutation_factors.append(
-                    f - (modified_population.members[i].fitness_value - origin_population.members[i].fitness_value) /
-                    modified_population.members[i].fitness_value * random.random())
-
-                crossover_rates.append(
-                    cr - (modified_population.members[i].fitness_value - origin_population.members[i].fitness_value) /
-                    modified_population.members[i].fitness_value * random.random())
+                mutation_factors[i] = [mutation_factors[i][0] - (
+                        modified_population.members[i].fitness_value - origin_population.members[i].fitness_value) /
+                                       modified_population.members[i].fitness_value * random.random(), True]
+                crossover_rates[i] = [crossover_rates[i][0] - (
+                        modified_population.members[i].fitness_value - origin_population.members[i].fitness_value) /
+                                      modified_population.members[i].fitness_value * random.random(), True]
             else:
                 new_members.append(copy.deepcopy(modified_population.members[i]))
 
@@ -55,15 +98,21 @@ def aade_selection(origin_population: Population, modified_population: Populatio
     return new_population
 
 
-def aade_adapat_parameters(mutation_factors: list[float], crossover_rates: list[float]) -> tuple[float, float]:
-    min_f = min(mutation_factors)
-    max_f = max(mutation_factors)
-    tmp_f = (max_f - min_f) / max_f
-    mutation_factors.clear()
+def aade_adapat_parameters(population: Population, mutation_factors: list[list[float, bool]],
+                           crossover_rates: list[list[float, bool]]):
+    sorted_members = population.get_best_members(population.size)
+    max_fitness_value = sorted_members[-1].fitness_value
+    min_fitness_value = sorted_members[0].fitness_value
 
-    min_cr = min(crossover_rates)
-    max_cr = max(crossover_rates)
-    tmp_cr = (max_cr - min_cr) / max_cr
-    crossover_rates.clear()
+    for i in range(population.size):
+        if mutation_factors[i][1]:
+            mutation_factors[i][1] = False
+        else:
+            mutation_factors[i] = [random.random() * ((max_fitness_value - min_fitness_value) / max_fitness_value),
+                                   False]
 
-    return random.random() * tmp_f, random.random() * tmp_cr
+        if crossover_rates[i][1]:
+            crossover_rates[i][1] = False
+        else:
+            crossover_rates[i] = [random.random() * ((max_fitness_value - min_fitness_value) / max_fitness_value),
+                                  False]
