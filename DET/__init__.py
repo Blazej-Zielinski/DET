7b749a40fbcec9cc8c8bed9ce725f1cd1a2c4167
Blazej-Zielinski <blazejos1230@gmail.com>
2024-10-06 19:24:11 +0200

    Repo rename

--- DET/__init__.py
+++ DET/__init__.py
@@ -1,15 +1,15 @@
-from diffEvoLib.diffEvoAlgs.de import DE
-from diffEvoLib.diffEvoAlgs.comde import COMDE
-from diffEvoLib.diffEvoAlgs.derl import DERL
-from diffEvoLib.diffEvoAlgs.nmde import NMDE
-from diffEvoLib.diffEvoAlgs.sade import SADE
-from diffEvoLib.diffEvoAlgs.emde import EMDE
-from diffEvoLib.diffEvoAlgs.ide import IDE
+from DET.DETAlgs.de import DE
+from DET.DETAlgs.comde import COMDE
+from DET.DETAlgs.derl import DERL
+from DET.DETAlgs.nmde import NMDE
+from DET.DETAlgs.sade import SADE
+from DET.DETAlgs.emde import EMDE
+from DET.DETAlgs.ide import IDE
 
-from diffEvoLib.diffEvoAlgs.data.alg_data import DEData, COMDEData, DERLData, NMDEData,\
+from DET.DETAlgs.data.alg_data import DEData, COMDEData, DERLData, NMDEData,\
     SADEData, EMDEData, IDEData
 
-from diffEvoLib.models.enums.optimization import OptimizationType
-from diffEvoLib.models.enums.boundary_constrain import BoundaryFixing
+from DET.models.enums.optimization import OptimizationType
+from DET.models.enums.boundary_constrain import BoundaryFixing
 
-from diffEvoLib.models.fitness_function import FitnessFunctionBase, FitnessFunction, FitnessFunctionOpfunu
+from DET.models.fitness_function import FitnessFunctionBase, FitnessFunction, FitnessFunctionOpfunu
