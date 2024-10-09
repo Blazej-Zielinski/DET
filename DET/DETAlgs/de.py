0ead1f553a4fa078ee4004448cb975cf08c008e0
TheOrboy <hubert.orlicki@petra-po.pl>
2024-10-09 19:07:53 +0200

    add default parameters for DET.DE

--- DET/DETAlgs/de.py
+++ DET/DETAlgs/de.py
@@ -2,10 +2,33 @@ from DET.DETAlgs.base import BaseAlg
 from DET.DETAlgs.data.alg_data import DEData
 from DET.DETAlgs.methods.methods_de import mutation, binomial_crossing, selection
 from DET.models.enums.boundary_constrain import fix_boundary_constraints
+from DET.models.fitness_function import FitnessFunctionOpfunu
+from DET.models.enums.optimization import OptimizationType
+from DET.models.enums.boundary_constrain import BoundaryFixing
 
-
+import opfunu.cec_based.cec2014 as opf
 class DE(BaseAlg):
-    def __init__(self, params: DEData, db_conn=None, db_auto_write=False):
+    def __init__(self, params: DEData = None, db_conn="Differential_evolution.db", db_auto_write=False):
+
+        fitness_fun_opf = FitnessFunctionOpfunu(
+            func_type=opf.F82014,
+            ndim=10
+        )
+
+        if params is None:
+            params = DEData(
+                epoch=100,
+                population_size=100,
+                dimension=10,
+                lb=[-5, -100, -100, -100, -100, -100, -100, -100, -100, -100],
+                ub=[5, 100, 100, 100, 100, 100, 100, 100, 100, 100],
+                mode=OptimizationType.MINIMIZATION,
+                boundary_constraints_fun=BoundaryFixing.RANDOM,
+                function=fitness_fun_opf,
+                log_population=True,
+                mutation_factor=0.5,
+                crossover_rate=0.5
+            )
         super().__init__(DE.__name__, params, db_conn, db_auto_write)
 
         self.mutation_factor = params.mutation_factor  # F
